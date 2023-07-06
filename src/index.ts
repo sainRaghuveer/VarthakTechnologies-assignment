1ed125151cac1e89e82467f8dbe033c01c477dca
sainRaghuveer <raghuveersain987@gmail.com>
2023-07-06 23:58:46 +0530

    created server at port 8080

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+import dotenv from "dotenv";
+dotenv.config() as string;
+const app = express();
+
+import connectDB from "./config/db";
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000 as number;
+
+app.listen(PORT, async () => {
+    try {
+        await connectDB();
+        console.log(`Connected with DB`);
+        console.log(`Server running prot ${PORT}`);
+    } catch (error) {
+        console.log(error);
+    }
+});
