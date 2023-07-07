969f039334e47cb6e391dcb453c158b826ed1d25
sainRaghuveer <raghuveersain987@gmail.com>
2023-07-07 14:35:57 +0530

    winston logger middleware for monitoring api calls

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,46 @@
-const express = require("express");
+import express from "express"
 const cors = require("cors");
 import dotenv from "dotenv";
 dotenv.config() as string;
+import winston from "winston";
+import {Request, Response, NextFunction} from "express"
 import userRouter from "./routes/auth.route"
 import bookRouter from "./routes/book.route"
+import connectDB from "./config/db";
+
+// Configure logger
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "logs/api.log" }),
+    ],
+});
+
+// Logging middleware
+const loggingMiddleware: express.Handler = (req:Request, res:Response, next:NextFunction) => {
+    logger.info(`[${req.method}] ${req.url}`);
+    next();
+};
+
+
 const app = express();
 
-import connectDB from "./config/db";
+
+app.get("/", (req, res) => {
+    res.send("Home page");
+});
 
 
 app.use(cors());
 app.use(express.json());
+
+//logger middleware
+app.use(loggingMiddleware);
 
 //Routes
 app.use("/", userRouter);
